008f9a0ee0dddaaf558ac189dbf0ae376feb2795
David Kirwan <davidkirwanirl@gmail.com>
2018-12-04 19:55:06 +0000

    no need for the 01_b_input.txt

--- 2018/01_b.rb
+++ 2018/01_b.rb
@@ -4,7 +4,7 @@ state_list = {}
 answer = nil
 
 while true
-File.foreach("01_b_input.txt").with_index do |line, line_num|
+File.foreach("01_a_input.txt").with_index do |line, line_num|
   #puts "#{state}, #{line}"
   state += line.to_i
   if state_list.has_key? state then answer = state; state_list[state] += 1; break; else state_list[state] = 1; end
