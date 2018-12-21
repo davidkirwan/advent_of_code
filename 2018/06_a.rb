341762f129ba0b9f55e85bb3c7686cad6b5da065
David Kirwan <dkirwan@redhat.com>
2018-12-21 16:53:31 +0000

    06 a wip

---
+++ 2018/06_a.rb
@@ -0,0 +1,11 @@
+# Sorted the input using VIM: shift+v, G, :'<,'>sort
+require "time"
+
+def process_input(input)
+  return {}
+end
+
+File.foreach("06_input.txt").with_index do |line, line_num|
+end
+
+puts "The answer should be #{output.length}"
