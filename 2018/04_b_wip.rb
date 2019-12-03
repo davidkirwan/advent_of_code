e89af8f4d8c3666fd8291b831886fa3d42cb9512
David Kirwan <dkirwan@redhat.com>
2019-12-03 22:20:14 +0000

    did some further processing on the 04 input make it easier to parse

--- 2018/04_b_wip.rb
+++ 2018/04_b_wip.rb
@@ -1,4 +1,14 @@
-# Sorted the input using VIM: shift+v, G, :'<,'>sort
+=begin 
+2018 04 b
+
+Sorted the input using vim commands:
+
+shift+v, G, :'<,'>sort
+%s/\[//g
+%s/\]//g
+%s/\(.\{-}\zs\s\)\{2}/,/g
+=end
+
 require "time"
 
 id = 0
@@ -19,10 +29,7 @@ ACTION_DESCRIPTION = {
 }
 
 def parse_lines(line)
-  l = line.split("]")
-  l[0].gsub!("[", "")
-  l[1].strip!
-
+  l = line.split(",")
   t = Time.strptime(l[0], '%Y-%m-%d %H:%M')
   
   if l[1].include?("Guard")
@@ -47,48 +54,7 @@ def parse_lines(line)
 end
 
 
-def process_events(id, event, guard_list)
-  unless guard_list.has_key?(id)
-    guard_list[id] = {
-      :events=>[],
-      :calendar=>Array.new(60,0)
-    }
-  end
-  guard_list[id][:events] << event
 
-  return true
-end
-
-
-def process_s(k, v)
-  puts "Key: #{k}, #{v[:events].size}"
-  
-  calendar = v[:calendar]
-  last_event = nil
-  total_minutes = 0
-
-  v[:events].each do |i|
-    #puts i.inspect
-    if i[:data][:action] == 1
-      if last_event[:data][:action] == 0
-	puts i[:time]
-	puts last_event[:time]
-	puts i[:time].min - last_event[:time].min
-	puts
-	start = last_event[:time].min
-	finish = i[:time].min
-
-	total_minutes += (finish-start)
-        start.upto(finish) do |x|
-	  calendar[x] += 1
-	end
-      end
-    end
-    v[:total_minutes] = total_minutes
-    puts calendar.inspect
-    last_event = i
-  end
-end
 
 
 File.foreach("04_input_sorted.txt").with_index do |line, line_num|
@@ -96,31 +62,6 @@ File.foreach("04_input_sorted.txt").with_index do |line, line_num|
   if parsed[:data][:action] == ACTION[:guard]
     id = parsed[:data][:id]
   end
-  puts "At #{parsed[:time]} #{id} #{ACTION_DESCRIPTION[parsed[:data][:action]]}"
-
-  process_events(id, parsed, guard_list)
+  puts "At #{parsed[:time]} guard: ##{id} #{ACTION_DESCRIPTION[parsed[:data][:action]]}"
 end
-
-guard_list.each do |k,v|
-  process_s(k, v)
-end
-
-guard_with_most_sleep_id = 0
-guard_with_most_sleep_on_target_minute = 0
-target_minute = 49
-
-
-guard_list.each do |k,v|
-  if guard_with_most_sleep_on_target_minute < v[:calendar][target_minute]
-    guard_with_most_sleep_id = k
-    guard_with_most_sleep_on_target_minute = v[:calendar][target_minute]
-  end
-  puts "Guard #{k}, was asleep: #{v[:calendar][49]} times on target minute"
-end
-
-
-puts "Guard asleep on target minute most is #{guard_with_most_sleep_id} with total of: #{guard_with_most_sleep_on_target_minute}"
-puts "Answer should be: #{target_minute * guard_with_most_sleep_id}"
-
-
 
